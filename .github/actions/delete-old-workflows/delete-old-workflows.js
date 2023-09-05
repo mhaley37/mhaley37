8b98d12157f5d5673cf13cb3772839205f1c5313
Michael Haley <mike.f.haley@gmail.com>
2023-09-05 11:18:04 -0400

    test stuff

--- .github/actions/delete-old-workflows/delete-old-workflows.js
+++ .github/actions/delete-old-workflows/delete-old-workflows.js
@@ -7,21 +7,22 @@ const deleteOldWorkflowsRuns = async () => {
     const { owner, repo } = github.context.repo;
     const common = {owner, repo}
     
-    const pulls = await octokit.rest.pulls.list(common)
-
-    const pull_branches = pulls.data.map( v => v.head.ref)
-    const workflows = await octokit.rest.repos.getContent({
+    const pull_branches = (await octokit.paginate(octokit.rest.pulls.list, common)).flat().map(v => v.head.ref)
+    //const pull_branches = pulls.data.map( v => v.head.ref)
+    const workflowPaths = await octokit.rest.repos.getContent({
       ...common,
       path: '.github/workflows'
-    })
-    const workflowPaths = workflows.data.map( d => d.path );
+    }).data.map( d => d.path );
+    //const workflowPaths = workflows.data.map( d => d.path );
 
     const runs = (await octokit.paginate(octokit.rest.actions.listWorkflowRunsForRepo, common)).flat()
 
     // TODO: Remove this
     const deletedWorkflows = [];
+    console.log('paths', JSON.stringify(workflowPaths))
     runs.forEach( run => {
       const {event, head_branch, id, path} = run;
+      console.log('Run:', JSON.stringify({event, head_branch, id, path}, null, 2))
       const alwaysKeep = head_branch == 'main' || event == 'release';
       const hasPR = !pull_branches.includes(head_branch);
       const activeWorkflow = workflowPaths.includes(path);
